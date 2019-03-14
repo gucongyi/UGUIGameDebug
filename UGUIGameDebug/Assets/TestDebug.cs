87b5f5e18cc90d8d32758af50ed9b0ef245742ff
gucongyi <412638119@qq.com>
2019-03-14 09:20:45 +0800

    InGameDebugLog

---
+++ UGUIGameDebug/Assets/TestDebug.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDebug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello");
+        Debug.LogWarning("Hello");
+        Debug.LogError("Hello");
+        Debug.Assert(1==2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
